cecfdf80476f4221c3ed342ad087b50203269db6
SamJ1005 <samjac7755@gmail.com>
2025-12-19 13:26:48 +0530

    updated echo files

--- Echo Receiver.py
+++ Echo Receiver.py
@@ -2,12 +2,13 @@ import socket
 
 s = socket.socket()
 s.connect(("localhost", 7000))
+print("Connected to server")
 
 while True:
-    m = input()
-    s.send(m.encode())
-    if m == "exit":
+    msg = input("Client: ")
+    s.send(msg.encode())
+    if msg == "exit":
         break
-    print(s.recv(1024).decode())
+    print("Server:", s.recv(1024).decode())
 
 s.close()
--- Echo Sender.py
+++ Echo Sender.py
@@ -4,13 +4,16 @@ s = socket.socket()
 s.bind(("localhost", 7000))
 s.listen(1)
 
+print("Server started. Waiting for client...")
 c, a = s.accept()
+print("Client connected:", a)
 
 while True:
-    m = c.recv(1024).decode()
-    if m == "exit":
+    msg = c.recv(1024).decode()
+    if msg == "exit":
         break
-    c.send(m.encode())
+    print("Client:", msg)
+    c.send(msg.encode())   # echo back
 
 c.close()
 s.close()
