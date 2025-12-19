7e38be015dbe5a544af486df47a283b05b53f562
SamJ1005 <samjac7755@gmail.com>
2025-12-19 13:47:15 +0530

    updated crc code error

--- CRC Error.py
+++ CRC Error.py
@@ -1,35 +1,66 @@
 # CRC-CCITT (16-bit)
 
-def crc_ccitt(data):
-    crc = 0xFFFF          # initial value
-    poly = 0x1021         # CRC-CCITT polynomial
-
-    for byte in data:
-        crc ^= ord(byte) << 8
-        for _ in range(8):
-            if crc & 0x8000:
-                crc = (crc << 1) ^ poly
-            else:
-                crc = crc << 1
-            crc &= 0xFFFF
-    return crc
+def xor(a, b):
+    result = ""
+    for i in range(1, len(b)):
+        result += '0' if a[i] == b[i] else '1'
+    return result
 
 
-# ---------- Sender ----------
-msg = input("Enter message: ")
-crc = crc_ccitt(msg)
-print("CRC value:", hex(crc))
+def crc_ccitt(data, key):
+    key_len = len(key)
+    temp = data[:key_len]
 
-# attach crc to message
-sent_data = msg + str(crc)
-print("Data sent:", sent_data)
+    for i in range(key_len, len(data)):
+        if temp[0] == '1':
+            temp = xor(key, temp) + data[i]
+        else:
+            temp = xor('0' * key_len, temp) + data[i]
+
+    if temp[0] == '1':
+        temp = xor(key, temp)
+    else:
+        temp = xor('0' * key_len, temp)
+
+    return temp
 
 
-# ---------- Receiver ----------
-recv_msg = msg   # assume correct data received
-recv_crc = crc_ccitt(recv_msg)
+# ---------------- Sender ----------------
+data = input("Enter data bits: ")
+key = "10001000000100001"   # CRC-CCITT polynomial
 
-if recv_crc == crc:
-    print("No error detected")
-else:
+appended_data = data + '0' * 16
+crc = crc_ccitt(appended_data, key)
+transmitted_data = data + crc
+
+print("\nSender Side")
+print("CRC:", crc)
+print("Transmitted Data:", transmitted_data)
+
+
+# ---------------- Receiver (NO ERROR) ----------------
+print("\nReceiver Side (Without Error)")
+received_data = transmitted_data
+
+check = crc_ccitt(received_data, key)
+if '1' in check:
     print("Error detected")
+else:
+    print("No error detected")
+
+
+# ---------------- Receiver (WITH ERROR) ----------------
+print("\nReceiver Side (With Error)")
+
+# Flip one bit to introduce error
+received_data_error = list(transmitted_data)
+received_data_error[5] = '1' if received_data_error[5] == '0' else '0'
+received_data_error = ''.join(received_data_error)
+
+print("Received Data (with error):", received_data_error)
+
+check_error = crc_ccitt(received_data_error, key)
+if '1' in check_error:
+    print("Error detected")
+else:
+    print("No error detected")
