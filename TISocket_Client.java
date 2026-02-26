9a0fe9826dcf5ae4983e1b0674ebbaae8ca82da5
SamJ1005 <196654975+SamJ1005@users.noreply.github.com>
2026-02-26 20:45:49 +0530

    New file socket and dijkstra's algorithm

---
+++ TISocket_Client.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class TISocket_Client {
+    public static void main(String[] args) throws Exception {
+
+        Socket s = new Socket("localhost", 5000);
+
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        String fileName = sc.nextLine();
+
+        dos.writeUTF(fileName);
+
+        try {
+            while (true) {
+                String data = dis.readUTF();
+                System.out.println(data);
+            }
+        } catch (EOFException e) {
+            System.out.println("End of file");
+        }
+
+        s.close();
+        sc.close();
+    }
+}
---
+++ TISocket_Server.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class TISocket_Server {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket ss = new ServerSocket(5000);
+        System.out.println("Server waiting...");
+
+        Socket s = ss.accept();
+        System.out.println("Client connected");
+
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        String fileName = dis.readUTF();
+
+        File file = new File(fileName);
+
+        if (file.exists()) {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                dos.writeUTF(line);
+            }
+            br.close();
+        } else {
+            dos.writeUTF("File not found");
+        }
+
+        s.close();
+        ss.close();
+    }
+}
